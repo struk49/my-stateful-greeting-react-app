c11bb840e4623c752ddf2f16917cad529d573d3a
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-15 08:58:25 +0000

    stateful react app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+
+function App() {
+  return (
+    <div className="App">
+      <StatefulGreetingWithPrevState />
+    </div>
+  );
+}
+
+export default App;
